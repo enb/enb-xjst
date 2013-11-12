08e2ffb029c6cf5a8224f33745731851c16b12ea
Andrew Abramov <andrewblond@yandex.com>
2013-11-12 18:31:42 +0400

    Refactoring tests

--- test/func/test.bemhtml.js
+++ test/func/test.bemhtml.js
@@ -5,18 +5,21 @@ require('chai').should();
 describe('functional', function() {
     describe('bemhtml', function() {
         describe('page', function() {
-            it('build simple page', function() {
-                var BEMHTML = {
-                        dev: require('../fixtures/bemhtml/page/page.dev.bemhtml').BEMHTML,
-                        prod: require('../fixtures/bemhtml/page/page.prod.bemhtml').BEMHTML
-                    },
+            it('build simple page in dev mode', function() {
+                var BEMHTML = require('../fixtures/bemhtml/page/page.dev.bemhtml').BEMHTML,
                     bemjson = require('../fixtures/bemhtml/data/page.json'),
                     html = fs.readFileSync('./test/fixtures/bemhtml/result/page.html', 'utf8');
 
-                BEMHTML.dev.apply(bemjson).should.equal(html);
-                BEMHTML.prod.apply(bemjson).should.equal(html);
+                BEMHTML.apply(bemjson).should.equal(html);
             });
 
+            it('build simple page in production mode', function() {
+                var BEMHTML = require('../fixtures/bemhtml/page/page.prod.bemhtml').BEMHTML,
+                    bemjson = require('../fixtures/bemhtml/data/page.json'),
+                    html = fs.readFileSync('./test/fixtures/bemhtml/result/page.html', 'utf8');
+
+                BEMHTML.apply(bemjson).should.equal(html);
+            });
         });
     });
 });
--- test/func/test.bemtree.js
+++ test/func/test.bemtree.js
@@ -2,19 +2,22 @@ require('chai').should();
 
 describe('functional', function() {
     describe('bemtree', function() {
-        it('should builds simple view of page', function() {
-            var BEMTREE = {
-                    dev: require('../fixtures/bemtree/page/page.dev.bemtree.xjst').BEMTREE,
-                    prod: require('../fixtures/bemtree/page/page.prod.bemtree.xjst').BEMTREE
-                },
+        it('should builds simple view of page in dev mode', function() {
+            var BEMTREE = require('../fixtures/bemtree/page/page.dev.bemtree.xjst').BEMTREE,
                 data = require('../fixtures/bemtree/data/data.json'),
                 view = JSON.stringify(require('../fixtures/bemtree/result/view.json'));
 
-            BEMTREE.dev.apply(data).then(function(res) {
+            BEMTREE.apply(data).then(function(res) {
                 JSON.stringify(res).should.equal(view);
             }).done();
+        });
 
-            BEMTREE.prod.apply(data).then(function(res) {
+        it('should builds simple view of page in production mode', function() {
+            var BEMTREE = require('../fixtures/bemtree/page/page.prod.bemtree.xjst').BEMTREE,
+                data = require('../fixtures/bemtree/data/data.json'),
+                view = JSON.stringify(require('../fixtures/bemtree/result/view.json'));
+
+            BEMTREE.apply(data).then(function(res) {
                 JSON.stringify(res).should.equal(view);
             }).done();
         });
